643e1982006856ef0ca527476de4052b8d5216d2
szilvagyivilmos <vilmos.szilvagyi@gmail.com>
2017-09-22 22:10:33 +0200

    asd

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hell o!");
+        System.out.println("He ll o!");
+        System.out.println("and fuck you!");
     }
 }
